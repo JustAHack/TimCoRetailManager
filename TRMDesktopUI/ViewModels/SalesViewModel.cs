3c01c6bd51839a93e8922a05ae577f5ab5acc95e
Thomas Kauran <tkauran@gmail.com>
2021-07-04 05:39:26 +1000

    Add Sales Page
    Adds the sales page xaml and supporting
    view model. Just scaffolds the VM
    does not add actual logic.

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,128 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+	public class SalesViewModel : Screen
+	{
+		private BindingList<string> _products;
+		private string _itemQuantity;
+		private BindingList<string> _cart;
+
+		public BindingList<string> Products
+		{
+			get => _products;
+			set
+			{
+				_products = value;
+				NotifyOfPropertyChange(() => Products);
+			}
+		}
+
+		public string ItemQuantity
+		{
+			get => _itemQuantity;
+			set
+			{
+				_itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+			}
+		}
+
+		public BindingList<string> Cart
+		{
+			get => _cart;
+			set
+			{
+				_cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+		public string SubTotal
+		{
+			get
+			{
+				// TODO - replace with calculation
+				return "$0.00";
+			}
+		}
+
+		public string Total
+		{
+			get
+			{
+				// TODO - replace with calculation
+				return "$0.00";
+			}
+		}
+
+		public string Tax
+		{
+			get
+			{
+				// TODO - replace with calculation
+				return "$0.00";
+			}
+		}
+
+
+		public bool CanAddToCart
+		{
+			get
+			{
+				bool output = false;
+
+				// Make sure something is selected
+				// Make sure the item has a quantity greater than 0
+
+				return output;
+			}
+		}
+
+		public bool CanRemoveFromCart
+		{
+			get
+			{
+				bool output = false;
+
+				// Make sure something is selected
+
+				return output;
+			}
+		}
+
+		public bool CanCheckout
+		{
+			get
+			{
+				bool output = false;
+
+				// Make sure something is in the cart
+
+				return output;
+			}
+		}
+
+		public void AddToCart()
+		{
+
+		}
+
+		public void RemoveFromCart()
+		{
+
+		}
+
+		public void Checkout()
+		{
+
+		}
+
+	}
+}
