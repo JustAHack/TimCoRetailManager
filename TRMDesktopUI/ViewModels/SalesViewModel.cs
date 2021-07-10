552d36719e2fa49a3d60d87addff763c6e155b9c
Thomas Kauran <tkauran@gmail.com>
2021-07-11 04:52:09 +1000

    Get add to cart working
    Adds the functionality to add items to the cart,
    get the subtotal and update quantities

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -13,8 +13,9 @@ namespace TRMDesktopUI.ViewModels
 	public class SalesViewModel : Screen
 	{
 		private BindingList<ProductModel> _products;
-		private BindingList<ProductModel> _cart;
-		private int _itemQuantity;
+		private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+		private ProductModel _selectedProduct;
+		private int _itemQuantity = 1;
 		private IProductEndpoint _productEndpoint;
 
 		public SalesViewModel(IProductEndpoint productEndpoint)
@@ -44,6 +45,18 @@ namespace TRMDesktopUI.ViewModels
 			}
 		}
 
+		public ProductModel SelectedProduct
+		{
+			get { return _selectedProduct; }
+			set
+			{
+				_selectedProduct = value;
+				NotifyOfPropertyChange(() => SelectedProduct);
+				NotifyOfPropertyChange(() => CanAddToCart);
+			}
+		}
+
+
 		public int ItemQuantity
 		{
 			get => _itemQuantity;
@@ -51,10 +64,11 @@ namespace TRMDesktopUI.ViewModels
 			{
 				_itemQuantity = value;
 				NotifyOfPropertyChange(() => ItemQuantity);
+				NotifyOfPropertyChange(() => CanAddToCart);
 			}
 		}
 
-		public BindingList<ProductModel> Cart
+		public BindingList<CartItemModel> Cart
 		{
 			get => _cart;
 			set
@@ -69,7 +83,14 @@ namespace TRMDesktopUI.ViewModels
 			get
 			{
 				// TODO - replace with calculation
-				return "$0.00";
+				decimal subtotal = 0;
+
+				foreach (var item in Cart)
+				{
+					subtotal += (item.Product.RetailPrice * item.QuantityInCart);
+				}
+
+				return subtotal.ToString("C");
 			}
 		}
 
@@ -100,6 +121,10 @@ namespace TRMDesktopUI.ViewModels
 
 				// Make sure something is selected
 				// Make sure the item has a quantity greater than 0
+				if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+				{
+					output = true;
+				}
 
 				return output;
 			}
@@ -112,6 +137,7 @@ namespace TRMDesktopUI.ViewModels
 				bool output = false;
 
 				// Make sure something is selected
+
 
 				return output;
 			}
@@ -131,12 +157,32 @@ namespace TRMDesktopUI.ViewModels
 
 		public void AddToCart()
 		{
+			CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
+			if (existingItem != null)
+			{
+				existingItem.QuantityInCart += ItemQuantity;
+				Cart.Remove(existingItem);
+				Cart.Add(existingItem);
+			}
+			else
+			{
+				CartItemModel item = new CartItemModel
+				{
+					Product = SelectedProduct,
+					QuantityInCart = ItemQuantity
+				};
+				Cart.Add(item);
+			}
+
+			SelectedProduct.QuantityInStock -= ItemQuantity;
+			ItemQuantity = 1;
+			NotifyOfPropertyChange(() => SubTotal);
 		}
 
 		public void RemoveFromCart()
 		{
-
+			NotifyOfPropertyChange(() => SubTotal);
 		}
 
 		public void Checkout()
