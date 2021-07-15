a986239313e7861173fd8c7d1affb627d748d063
Thomas Kauran <tkauran@gmail.com>
2021-07-16 04:40:37 +1000

    Added remove and add roles functionality
    Added remove and add roles for user
    administration

--- TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -18,6 +18,12 @@ namespace TRMDesktopUI.ViewModels
 		private readonly IWindowManager _window;
 		private readonly IUserEndpoint _userEndpoint;
 		private BindingList<UserModel> _users;
+		private UserModel _selectedUser;
+		private string _selectedUserName;
+		private BindingList<string> _userRoles = new BindingList<string>();
+		private BindingList<string> _availableRoles = new BindingList<string>();
+		private string _selectedUserRole;
+		private string _selectedAvailableRole;
 
 		public BindingList<UserModel> Users
 		{
@@ -30,6 +36,69 @@ namespace TRMDesktopUI.ViewModels
 			{
 				_users = value;
 				NotifyOfPropertyChange(() => Users);
+			}
+		}
+
+		public UserModel SelectedUser
+		{
+			get { return _selectedUser; }
+			set
+			{
+				_selectedUser = value;
+				SelectedUserName = value.Email;
+				UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+				LoadRoles().GetAwaiter();
+				NotifyOfPropertyChange(() => SelectedUser);
+			}
+		}
+
+		public string SelectedUserName
+		{
+			get { return _selectedUserName; }
+			set
+			{
+				_selectedUserName = value;
+				NotifyOfPropertyChange(() => SelectedUserName);
+			}
+		}
+
+		public BindingList<string> UserRoles
+		{
+			get { return _userRoles; }
+			set
+			{
+				_userRoles = value;
+				NotifyOfPropertyChange(() => UserRoles);
+			}
+		}
+
+		public BindingList<string> AvailableRoles
+		{
+			get { return _availableRoles; }
+			set
+			{
+				_availableRoles = value;
+				NotifyOfPropertyChange(() => AvailableRoles);
+			}
+		}
+
+		public string SelectedUserRole
+		{
+			get { return _selectedUserRole; }
+			set
+			{
+				_selectedUserRole = value;
+				NotifyOfPropertyChange(() => SelectedUserRole);
+			}
+		}
+
+		public string SelectedAvailableRole
+		{
+			get { return _selectedAvailableRole; }
+			set
+			{
+				_selectedAvailableRole = value;
+				NotifyOfPropertyChange(() => SelectedAvailableRole);
 			}
 		}
 
@@ -73,6 +142,34 @@ namespace TRMDesktopUI.ViewModels
 		{
 			var userList = await _userEndpoint.GetAll();
 			Users = new BindingList<UserModel>(userList);
+		}
+
+		private async Task LoadRoles()
+		{
+			var roles = await _userEndpoint.GetAllRoles();
+
+			foreach (var role in roles)
+			{
+				if (UserRoles.IndexOf(role.Value) < 0)
+				{
+					AvailableRoles.Add(role.Value);
+				}
+			}
+		}
+
+		public async void AddSelectedRole()
+		{
+			await _userEndpoint.AddUserToRole(SelectedUser.Id, SelectedAvailableRole);
+
+			UserRoles.Add(SelectedAvailableRole);
+			AvailableRoles.Remove(SelectedAvailableRole);
+		}
+
+		public async void RemoveSelectedRole()
+		{
+			await _userEndpoint.RemoveUserFromRole(SelectedUser.Id, SelectedUserRole);
+			AvailableRoles.Add(SelectedUserRole);
+			UserRoles.Remove(SelectedUserRole);
 		}
 	}
 }
